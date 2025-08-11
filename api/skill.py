eea2b34a12438e3fa08c9421eefead668d55c36a
mindvr124 <mindvr124@gmail.com>
2025-08-11 14:24:54 +0900

    Update code

--- api/skill.py
+++ api/skill.py
@@ -3,8 +3,20 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 @app.post("/skill")
-async def basic(request: Request):
+async def skill(request: Request):
     req_data = await request.json()
-    utterance =  req_data['userRequest']['utterance']
-    print(utterance)
-    return {"content": "안녕"}
+    utterance = req_data["userRequest"]["utterance"]
+    print("사용자 발화:", utterance)
+
+    return {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": f"안녕하세요! '{utterance}' 라고 하셨네요."
+                    }
+                }
+            ]
+        }
+    }
